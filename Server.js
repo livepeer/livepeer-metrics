484faa644a1e751e566ccf8c4b47e99d7f14dedd
Eric Tang <eric.x.tang@gmail.com>
2018-06-24 18:43:03 -0400

    small bug fix

--- Server.js
+++ Server.js
@@ -1,5 +1,5 @@
 // server.js
-var app = require('./app');
+var app = require('./App');
 var port = process.env.PORT || 3000;
 var server = app.listen(port, function() {
   console.log('Express server listening on port ' + port);
