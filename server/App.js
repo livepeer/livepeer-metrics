4f6f176913f081bbe1861adaa2bb3314e5c7809e
Ivan Tivonenko <darkdarkdragon@gmail.com>
2018-08-09 01:26:03 +0300

    'v2' dir renamed to 'handlers'

--- server/App.js
+++ server/App.js
@@ -4,8 +4,8 @@
 const express = require('express');
 const app = express();
 const db = require('./DB');
-const eventsRouter = require('./v2/EventsController');
-const videoRouter = require('./v2/VideosController');
+const eventsRouter = require('./handlers/EventsController');
+const videoRouter = require('./handlers/VideosController');
 
 app.use('/api/events', eventsRouter);
 app.use('/api/videos', videoRouter);
